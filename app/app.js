359e3adbef8402adefbcc6ce9425fc4492804d52
Orestas <mazeikaorestas@gmail.com>
2017-10-26 13:19:58 +0300

    Login is working

--- app/app.js
+++ app/app.js
@@ -1,5 +1,21 @@
 // create the module and name it scotchApp    
-var dorm = angular.module('dormApp', ['ngRoute']);  
+var dorm = angular.module('dormApp', ['ngRoute']);
+
+dorm.controller('loginCtrl', function ($scope) {
+    $scope.submit = function () {
+        var uname = $scope.username;
+        var password = $scope.password;
+        if($scope.username == 'admin' && $scope.password == 'admin'){
+            window.location.href = "#/dashboard"
+            //$location.path('../views/dashboard.html');
+        }
+
+        else {
+            alert("Login information is incorrect!!!");
+        }
+    }
+
+})
 
 dorm.config(['$locationProvider', function($locationProvider) {
   $locationProvider.hashPrefix('');
@@ -10,5 +26,11 @@ dorm.config(['$routeProvider', function($routeProvider){
 	$routeProvider.when('/login', {
 		templateUrl: 'views/login.html'
 	})
+
+        .when('/dashboard', {
+            templateUrl: 'views/dashboard.html'
+        })
 }]);
+
+
 
